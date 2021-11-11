7479fbde2877cd460702012a909086ed5fd6683c
Abhinav Bhatia <abhinav.bhatia.me@gmail.com>
2021-11-11 18:24:17 -0500

    TSP optimizations

--- src/example_problems/tsp.jl
+++ src/example_problems/tsp.jl
@@ -1,19 +1,19 @@
 using ..SearchProblem
 using Random
 
-mutable struct TSP <: SearchProblem.AbstractSearchProblem{Vector{Int}}
+mutable struct TSP <: SearchProblem.AbstractSearchProblem{Vector{UInt8}}
     N_range::AbstractArray{Int}
-    sparsity_range::Tuple{Float32,Float32}
+    sparsity_range::Tuple{Float64,Float64}
     N::Int
-    state::Array{Int}
-    costs::Array{Float32,2}
-    heuristic_cache::Dict{Array{Int},Float32}
-    sparsity::Float32
+    state::Array{UInt8}
+    costs::Array{Float64,2}
+    heuristic_cache::Dict{Array{UInt8},Float64}
+    sparsity::Float64
     rng::MersenneTwister
     function TSP(N_range::AbstractArray{Int}, sparsity_range::Tuple{Real,Real})
         tsp = new(N_range, sparsity_range)
         tsp.rng = MersenneTwister()
-        tsp.heuristic_cache = Dict{Array{Int},Float32}()
+        tsp.heuristic_cache = Dict{Array{Int},Float64}()
         tsp.N = rand(tsp.rng, tsp.N_range)
         tsp.costs = fill(Inf32, tsp.N, tsp.N)
         return tsp
@@ -35,20 +35,20 @@ function SearchProblem.reset!(tsp::TSP)
     empty!(tsp.heuristic_cache)
     tsp.N = rand(tsp.rng, tsp.N_range)
     target_sparsity = tsp.sparsity_range[1] + (tsp.sparsity_range[2] - tsp.sparsity_range[1]) * rand(tsp.rng)
-    tsp.state = [1]
+    tsp.state = UInt8[0x01]
     tsp.costs = fill(Inf32, tsp.N, tsp.N)
 
     num_connections = 0
-    connected(cityid1, cityid2) = tsp.costs[cityid1, cityid2] < Inf
-    function connect!(cityid1, cityid2, min_distance=1, max_distance=50)
+    @inline connected(cityid1, cityid2) = tsp.costs[cityid1, cityid2] < Inf
+    @inline function connect!(cityid1, cityid2, min_distance=1, max_distance=50)
         if !connected(cityid1, cityid2)
-            distance = min_distance + rand(tsp.rng, Float32) *  (max_distance - min_distance)
+            distance = min_distance + rand(tsp.rng, Float64) *  (max_distance - min_distance)
             tsp.costs[cityid1, cityid2] = tsp.costs[cityid2, cityid1] = distance
             num_connections += 1
         end
     end
 
-    for cityid in 1:tsp.N
+    @inbounds for cityid in 1:tsp.N
         tsp.costs[cityid, cityid] = 0
         cityid > 1 ? connect!(cityid, cityid - 1) : nothing
         cityid == tsp.N ? connect!(cityid, 1) : nothing
@@ -64,15 +64,15 @@ function SearchProblem.reset!(tsp::TSP)
     tsp.sparsity = (max_connections - num_connections) / max_connections
 end
 
-@inline function SearchProblem.start_state(tsp::TSP)::Vector{Int}
+@inline function SearchProblem.start_state(tsp::TSP)::Vector{UInt8}
     return tsp.state
 end
 
-function SearchProblem.cost(tsp::TSP, state::Array{Int}, action::String, next_state::Array{Int})::Float64
-    return tsp.costs[state[end], next_state[end]]
+function SearchProblem.cost(tsp::TSP, state::Array{UInt8}, action::UInt8, next_state::Array{UInt8})::Float64
+    return @inbounds tsp.costs[state[end], next_state[end]]
 end
 
-@inline function SearchProblem.goal_test(tsp::TSP, state::Array{Int})::Bool
+@inline function SearchProblem.goal_test(tsp::TSP, state::Array{UInt8})::Bool
     if length(state) == tsp.N + 1
         return true
     else
@@ -80,31 +80,30 @@ end
     end
 end
 
-function mst_prims(costs::Array{Float32,2})::Float64
+function mst_prims(costs::Array{Float64,2})::Float64
     N = size(costs)[1]
     mst_nodes = Set{Int}()
-    key_vals::Array{Float32} = fill(Inf32, N)
+    key_vals::Array{Float64} = fill(Inf32, N)
     key_vals[1] = 0
     while length(mst_nodes) < N
         candidates = setdiff(1:N, mst_nodes)
-        candidate_values = [key_vals[c] for c in candidates]
+        candidate_values = @inbounds Float64[key_vals[c] for c in candidates]
         best_node = minimum(zip(candidate_values, candidates))[2]
         push!(mst_nodes, best_node)
-        adjacent_nodes = setdiff(filter(node -> 0 < costs[best_node, node] < Inf,  1:N), mst_nodes)
-        for adjacent in adjacent_nodes
-            key_vals[adjacent] = min(key_vals[adjacent], costs[best_node, adjacent])
+        adjacent_nodes::Vector{Int} = setdiff(filter(node -> 0 < costs[best_node, node] < Inf,  1:N), mst_nodes)
+        @simd for adjacent in adjacent_nodes
+            @inbounds key_vals[adjacent] =  ifelse(key_vals[adjacent] < costs[best_node, adjacent], key_vals[adjacent], costs[best_node, adjacent])
         end
     end
     mst_cost = sum(key_vals)
     return mst_cost
 end
 
-function SearchProblem.heuristic(tsp::TSP, state::Vector{Int})::Float64
-    h = 0
+function SearchProblem.heuristic(tsp::TSP, state::Vector{UInt8})::Float64
     if haskey(tsp.heuristic_cache, state)
         return tsp.heuristic_cache[state]
     else
-        h = 0
+        h::Float64 = 0
         mst_cities = Set(1:tsp.N)
 
         if length(state) >= 2
@@ -122,25 +121,21 @@ function SearchProblem.heuristic(tsp::TSP, state::Vector{Int})::Float64
     end
 end
 
-function SearchProblem.successors(tsp::TSP, state::Array{Int})
-    cur_city = state[end]
-    available_actions = Set()
-    if length(state) == tsp.N
+function SearchProblem.successors(tsp::TSP, state::Array{UInt8})
+    @inbounds cur_city = state[end]
+    @inbounds if length(state) == tsp.N
         if tsp.costs[1, cur_city] < Inf
-            push!(available_actions, 1)
+            available_actions = UnitRange{UInt8}(1, 1)
+        else
+            available_actions = UnitRange{UInt8}(1, 0) # empty range
         end
     else
-        adjacent = filter(c -> 0 < tsp.costs[cur_city, c] < Inf,  1:tsp.N)
-        union!(available_actions, adjacent)
-        setdiff!(available_actions, state)
+        available_actions = Iterators.filter(UnitRange{UInt8}(1, tsp.N)) do c
+            return @inbounds (0 < tsp.costs[cur_city, c] < Inf) && !(c in state)
+        end
     end
-    succ = Set{Tuple{Array{Int},String}}()
-    for a in available_actions
-        next_state = vcat(state, [a])
-        action = "goto $a"
-        push!(succ, (next_state, action))
+    return Iterators.map(available_actions) do action
+        next_state = vcat(state, action)
+        return next_state, action
     end
-    return succ
 end
-
-
