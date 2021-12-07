2fe2a1e4c8c99e887ec3fb3aced588024e853a7b
Abhinav Bhatia <abhinav.bhatia.me@gmail.com>
2021-12-07 17:49:03 +0530

    Bump version

--- scripts/solve_all.jl
+++ scripts/solve_all.jl
@@ -5,22 +5,22 @@ using Random
 
 function solve_all(search_problem, num_instances, rwa_weights, awa_weights, nodes_budget; walltime_limit=Inf)
     solution_qualities_per_approach = Dict{Any,Vector{Float64}}()
+    for w in [rwa_weights, awa_weights...]
+        solution_qualities_per_approach[w] = Float64[]
+    end
     for instance_id in 1:num_instances
         Random.seed!(search_problem, instance_id) 
         reset!(search_problem)
         h0 = heuristic(search_problem, start_state(search_problem))
-        for w in [rwa_weights, awa_weights...]
+        Threads.@threads for w in [rwa_weights, awa_weights...]
             if w == rwa_weights
                 awa = rwastar_search(search_problem, w, walltime_limit, nodes_budget, instance_id)
             else
                 awa = awastar_search(search_problem, w, walltime_limit, nodes_budget)
             end
             solution_quality = quality(awa.solution_cost, h0)
-            @info "Solved instance" instance_id h0 w awa.nodes_expended awa.num_solutions awa.solution_cost solution_quality SearchProblem.info(search_problem)...
+            @info "Solved instance" instance_id h0 w awa.nodes_expended awa.num_solutions awa.solution_cost solution_quality expansion_rate(awa) SearchProblem.info(search_problem)...
 
-            if !haskey(solution_qualities_per_approach, w)
-                solution_qualities_per_approach[w] = Float64[]
-            end
             push!(solution_qualities_per_approach[w], solution_quality)
         end
     end
@@ -37,14 +37,14 @@ end
 
 rwa_weights = [1, 1.5, 2, 3, 4, 5]  # Run rwa* with these weights
 awa_weights = [1, 1.5, 2, 3, 4, 5]  # Run awa* with each of these weights
-nodes_budget = 6000
+nodes_budget = 50000
 num_instances = 100
 
 
-search_problem = SlidingPuzzle(4:4, 35:45)  # Sliding Puzzle
-# search_problem = SlidingPuzzle(4:4, 35:45, inverse=true)  # Inverse Sliding Puzzle
+# search_problem = SlidingPuzzle(4:4, 40:50)  # Sliding Puzzle
+search_problem = SlidingPuzzle(4:4, 40:50, inverse=true)  # Inverse Sliding Puzzle
 # search_problem = CityNavigation(150, 150, 3, 3, 100, 1) # City Navigation Problem
-# search_problem = TSP(15:25, (0, 0.3))  # Sparse Travelling Salesman Problem
+# search_problem = TSP(25:35, (0.2, 0.8))  # Sparse Travelling Salesman Problem
 
 qualities_per_approach = solve_all(search_problem, num_instances, rwa_weights, awa_weights, nodes_budget)
 avg_quality_per_approach = get_avg_solution_quality_per_approach(qualities_per_approach)
