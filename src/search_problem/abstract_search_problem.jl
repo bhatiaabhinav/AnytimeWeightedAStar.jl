ae6bbec5caf9353ea887994f80abf9e135ff76f0
Abhinav Bhatia <abhinav.bhatia.me@gmail.com>
2021-12-18 02:49:40 -0500

    Instructions for creating new search problems

--- src/search_problem/abstract_search_problem.jl
+++ src/search_problem/abstract_search_problem.jl
@@ -6,7 +6,7 @@ Random.seed!(sp::AbstractSearchProblem, seed) = Random.seed!(sp.rng, seed)
 
 obs(sp::AbstractSearchProblem) = []
 
-info(sp::AbstractSearchProblem) = Dict{String,Any}()
+info(sp::AbstractSearchProblem) = Dict{String,Any}()  # Some nice printatble information about the search-problem instance e.g., number of cities in a Travelling-Salesman-Problem instance, or densitiy of obstacles in a grid-navigation-puzzle instance.
 
 reset!(sp::AbstractSearchProblem) = nothing
 
@@ -15,10 +15,10 @@ function start_state(sp::AbstractSearchProblem{S,A})::S where {S,A}
 end
 
 function successors(sp::AbstractSearchProblem{S,A}, state::S) where {S,A}
-    error("not implemented")
+    error("Not Implemented. Should return a list (or any iterable collection) of Tuple{S, A}. Each tuple is a pair of: next-state and the action that leads to that next-state from the given state.")
 end
 
-function cost(sp::AbstractSearchProblem{S}, state::S, action::A, next_state::S) where {S,A}
+function cost(sp::AbstractSearchProblem{S}, state::S, action::A, next_state::S)::Float64 where {S,A}
     error("not implemented")
 end
 
